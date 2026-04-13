e83e0941a27238be090c35d88e4d3a749d94bc17
hssaniwork-hash <hssaniwork@gmail.com>
2026-04-13 10:08:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function showmsg(){
+      const msg = document.querySelector(".msg");
+        msg.classList.add("show");
+        setTimeout(() => {
+            msg.classList.remove("show");
+        }, 5000);
+}
